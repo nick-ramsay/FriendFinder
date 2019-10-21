ba57ce417c0abf85dba9b42535d34bc8c49b956b
Nick Ramsay <NickRamsay1989@gmail.com>
2019-10-21 19:09:35 +1100

    Committing additional progress to repository

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,15 @@
-var friends = require("../data/friends.js");
+var friends = require("../data/friends.js");;
 
 module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
     console.log(friends.friends[0]);
     return res.json(friends.friends[0]);
   });
+
+  app.post("/api/friends", function (req, res) {
+    var newFriend = req.body;
+    //console.log(newFriend);
+    console.log(res.send);
+  });
+
 };
