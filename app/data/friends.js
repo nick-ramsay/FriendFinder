4639509a25032ffedd521178aca5f649d4e6e488
Nick Ramsay <NickRamsay1989@gmail.com>
2019-10-26 17:43:14 +1100

    Final changes for submission of project.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,7 @@
 var friends = [{
     "name": "Gary Eucalyptus",
     "photo": "https://www.abc.net.au/news/image/11546806-3x2-700x467.jpg",
+    "message": "I own multiple homes and by homes I mean trees.",
     "scores": [
         5,
         1,
@@ -13,6 +14,57 @@ var friends = [{
         4,
         1
     ]
-}];
+},
+{
+    "name": "Walter White",
+    "photo": "https://66.media.tumblr.com/11ffdd10843f0e9a3ad59d864eecf14b/tumblr_oeyupzabNU1qzpxx1o1_1280.jpg",
+    "message": "I hope your favorite color is also blue.",
+    "scores": [
+        3,
+        5,
+        4,
+        3,
+        2,
+        4,
+        1,
+        1,
+        3,
+        5
+    ]},
+    {
+        "name": "The Most Interesting Man in the World",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/701/most-interesting-man-goldsmith-1504044751.jpg?resize=480:*",
+        "message": "I don't usually drink beer but, when I do, I'd prefer to drink with you.",
+        "scores": [
+            1,
+            2,
+            3,
+            3,
+            3,
+            2,
+            3,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Hannibal Lecter",
+        "photo": "https://static3.comicvine.com/uploads/original/11130/111309754/5733118-b1d165c6-2794-4d34-9863-c711ff4c5819.jpg",
+        "message": "Are you free for dinner?",
+        "scores": [
+            2,
+            3,
+            3,
+            3,
+            3,
+            2,
+            3,
+            2,
+            2,
+            3
+        ]
+    }
+];
 
 module.exports.friends = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,37 @@
 module.exports = function (app) {
   var friends = require("../data/friends.js");
   var path = require("path");
-  var bodyParser = require('body-parser')
 
   app.get("/api/friends", function (req, res) {
-    console.log(friends.friends[0]);
-    return res.json(friends.friends[0]);
+    return res.json(friends.friends);
   });
 
-  app.post("/api/friends", function (req,res) {
-    // console.log(req);
+  app.post("/api/friends", function (req, res) {
     var newFriendData = req.body;
-    console.log(req.body);
-    // console.log(newFriendData);
-    // console.log("apiRoutes.js post...");
-    res.send(req.data)
+    var optimalFriend = -1;
+    var optimalFriendScore = 0;
+    var currentFriendScore;
+    for (i = 0; i < friends.friends.length; i++) {
+      currentFriendScore = 0;
+      for (s = 0; s < friends.friends[i].scores.length; s++) {
+        currentFriendScore += Math.abs(friends.friends[i].scores[s] - newFriendData.newFriend.scores[s]);
+        //currentFriendScore += friends.friends[i].scores[s];
+      };
+      switch (i) {
+        case 0:
+          optimalFriend = i;
+          optimalFriendScore = currentFriendScore;
+          break;
+        default:
+          switch (currentFriendScore < optimalFriendScore) {
+            case true:
+              optimalFriend = i;
+              optimalFriendScore = currentFriendScore;
+              break;
+          };
+      };
+    };
+    res.json(friends.friends[optimalFriend]);
   });
 
 };
